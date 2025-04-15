38f4928f815898dca1b6cbce2c0e0df378e290c0
salman-ctrl <zirmanvictory@gmail.com>
2025-04-15 14:36:40 +0700

    tambah halaman contact

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    loadComponent("header-container", "Header.html", setupMenu);
-    loadComponent("footer-container", "Footer.html");
-  });
-  
-  function loadComponent(containerId, file, callback) {
-    fetch(file)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById(containerId).innerHTML = data;
-        if (callback) callback(); 
-      })
-      .catch(error => console.error("Error loading " + file, error));
+  loadComponent("header-container", "Header.html", setupMenu);
+  loadComponent("footer-container", "Footer.html");
+});
+
+function loadComponent(containerId, file, callback) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(containerId).innerHTML = data;
+      if (callback) callback();
+    })
+    .catch(error => console.error("Error loading " + file, error));
+}
+
+function setupMenu() {
+  const btn = document.getElementById("menu-btn");
+  const menu = document.getElementById("nav-menu");
+
+  if (btn && menu) {
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
   }
-  
-  function setupMenu() {
-    const btn = document.getElementById("menu-btn");
-    const menu = document.getElementById("nav-menu");
-  
-    if (btn && menu) {
-      btn.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-      });
-    }
-  }
+}
